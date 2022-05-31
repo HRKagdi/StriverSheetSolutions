4067b660ba7414dcba9babf83eca70c9f214e61c
HRKagdi <53858937+HRKagdi@users.noreply.github.com>
2022-05-31 19:02:02 +0530

    Create majorityElement2.cpp

---
+++ Day3/majorityElement2.cpp
@@ -0,0 +1,14 @@
+
+//https://leetcode.com/problems/majority-element-ii/
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+         map<int,int> m; vector<int> v;
+        for(int i=0;i<nums.size();i++) m[nums[i]]++;
+        for(auto it:m){
+            if(it.second>nums.size()/3) v.push_back(it.first); 
+        }
+        return v;
+    }
+};
