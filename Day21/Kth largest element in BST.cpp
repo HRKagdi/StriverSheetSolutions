e446c003a537fb042e2667ea83a99650799c7f53
HRKagdi <53858937+HRKagdi@users.noreply.github.com>
2022-06-17 18:53:13 +0530

    Create  Kth largest element in BST.cpp

---
+++ Day21/Kth largest element in BST.cpp
@@ -0,0 +1,21 @@
+//https://practice.geeksforgeeks.org/problems/kth-largest-element-in-bst/1
+
+
+
+class Solution
+{
+    public:
+    void inorder(Node*root,vector<int>&v){
+        if(root==NULL) return;
+        inorder(root->left,v);
+        v.push_back(root->data);
+        inorder(root->right,v);
+    }
+    int kthLargest(Node *root, int K)
+    {
+        vector<int> v; 
+        inorder(root,v);
+        return v[v.size()-K];
+        
+    }
+};
