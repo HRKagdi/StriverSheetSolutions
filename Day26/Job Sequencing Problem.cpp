75034a396a4b711b67ac8ac8cf2e5236a6dec179
HRKagdi <53858937+HRKagdi@users.noreply.github.com>
2022-06-30 22:18:35 +0530

    Create  Job Sequencing Problem.cpp

---
+++ Day26/Job Sequencing Problem.cpp
@@ -0,0 +1,29 @@
+//https://practice.geeksforgeeks.org/problems/job-sequencing-problem-1587115620/1
+
+
+
+
+class Solution 
+{
+    public:
+    bool static comparator(Job a, Job b){
+        return (a.profit>b.profit);
+
+    }
+    vector<int> JobScheduling(Job arr[], int n) 
+    { 
+        sort(arr,arr+n,comparator);
+        int nd,pro; nd=pro=0;
+        vector<bool>deadline(n,false);
+        for(int i=0;i<n;i++){
+            for(int j=arr[i].dead;j>0;j--){
+                if(!deadline[j]){
+                    pro+=arr[i].profit; nd++;
+                    deadline[j]=true; break;
+                }
+            }
+        }
+        vector<int>v(2); v[0]=nd; v[1]=pro;
+        return v;
+    }  
+};
